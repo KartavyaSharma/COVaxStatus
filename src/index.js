a899f55616ec885235d2fc7e77f34c6b99b8cce8
KartavyaSharma <Kartavya@outlook.com>
2021-05-22 23:36:09 +0100

    feat: Added fonts + basic frontend

--- src/index.js
+++ src/index.js
@@ -9,8 +9,15 @@ export default function IndexPage() {
     const district = "Indore";
     const date = "23-05-2021";
     return (
-        <div>
-            
+        <div className='bg-violet-50 min-h-screen'>
+            <div className='pt-8 md:pt-12 lg:pt-16 lg:pb-24 px-6 md:px-8 lg:px-12 xl:px-20 2xl:px-36 max-w-screen-2xl w-full mx-auto font-head flex flex-row items-center'>
+            	<div className='text-4xl w-1/2'>
+            		<span className='font-bold'>COWIN</span> <span className='italic'>Updates</span>
+            	</div>
+            	<div className='flex flex-row justify-end w-full'>
+	            	<div className='font-head opacity-80 text-lg'>About</div>
+	            </div>
+            </div>
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+    	fontFamily: {
+    		'heads': ['Chivo', 'Helvetica', 'Arial', 'sans-serif'],
+    	},
+    	colors: {
+    		violet: colors.violet,
+    	},
+    },
   },
   variants: {
     extend: {},
